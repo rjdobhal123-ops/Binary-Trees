664f530154b1cf74d49c0defcf82ca16b4b04c20
Rajat Dobhal <rjdobhal123@gmail.com>
2026-04-05 15:10:33 +0530

    added levedl order traversal

--- src/Binary_tree_implementation.java
+++ src/Binary_tree_implementation.java
@@ -106,4 +106,29 @@ class Binary_tree_implementation {
         inorder(root.right,list);
 
     }
+
+    //Level-order traversal
+    public List<List<Integer>> levelorderTraversal(Node root){
+        List<List<Integer>> ans=new ArrayList<>();
+        if(root==null)
+            return ans;
+
+        Queue<Node> q=new ArrayDeque<>();
+        q.add(root);
+        while (!q.isEmpty()){
+            int size=q.size();
+            List<Integer> level=new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+                Node node = q.poll();
+                level.add(node.value);
+                if (node.left != null)
+                    q.add(node.left);
+                if (node.right != null)
+                    q.add(node.right);
+            }
+            ans.add(level);
+        }
+        return ans;
+    }
 }
