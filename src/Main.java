732181c26b995a49578d84be59a602ddb46c2c06
Rajat Dobhal <rjdobhal123@gmail.com>
2026-04-05 15:11:24 +0530

    added level order traversal

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Binary_tree_implementation bt=new Binary_tree_implementation();
-        bt.insert(new Scanner(System.in));
+//        bt.insert(new Scanner(System.in));
+        //      5
+        //     / \
+        //   12   13
+        //   /  \    \
+        //  7    14   2
+        // / \  /  \  / \
+        //17 23 27 3  8  11
+
 //        bt.display();
 //        System.out.println( bt.preorderTraversal(bt.root));
 
