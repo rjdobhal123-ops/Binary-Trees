cd989d31b4993a5224433e4e9054ea014cf41de1
Rajat Dobhal <rjdobhal123@gmail.com>
2026-04-07 09:38:05 +0530

    Added all traversals

--- src/Binary_tree_implementation.java
+++ src/Binary_tree_implementation.java
@@ -1,3 +1,4 @@
+import java.security.KeyPair;
 import java.util.*;
 
 class Binary_tree_implementation {
@@ -239,5 +240,53 @@ class Binary_tree_implementation {
             }
         }
         return postorder;
+    }
+
+
+    //Pre,Post and In in one traversal
+    public void traversal3in1(Node root){
+         if (root==null)
+             return;
+
+        class Pair {
+            Node node;
+            int num;
+
+            public Pair(Node node, int num) {
+                this.node = node;
+                this.num = num;
+            }
+        }
+
+         Stack<Pair> st=new Stack<>();
+         List<Integer> pre=new ArrayList<>();
+        List<Integer> in=new ArrayList<>();
+        List<Integer> post=new ArrayList<>();
+
+        st.push(new Pair(root,1));
+
+        while(!st.isEmpty()){
+            Pair it=st.pop();
+
+            if (it.num==1){
+                pre.add(it.node.value);
+                it.num++;
+                st.push(it);
+
+                if (it.node.left!=null){
+                    st.push(new Pair(it.node.left,1));
+                }
+            } else if (it.num==2) {
+                in.add(it.node.value);
+                it.num++;
+                st.push(it);
+
+                if (it.node.right!=null){
+                    st.push(new Pair(it.node.right,1));
+                }
+            }else{
+                post.add(it.node.value);
+            }
+        }
     }
 }
